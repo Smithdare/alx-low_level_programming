ddd619472e2e983ac80e75285615b595c739cba5
Smith dare <smithdare279@gmail.com>
2022-06-02 20:08:29 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - Entry point 
+*
+* Return: Always (Success)
+*/
+int main(void)
+{
+	printf("with propergrammer, but the outcome is a piece of art,\n");
+	return (0);
+}
