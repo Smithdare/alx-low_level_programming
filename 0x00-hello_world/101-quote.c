b803efac473bd6a0add7d025fd01fbd9a82db8dc
Smith dare <smithdare279@gmail.com>
2022-06-03 03:17:07 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main _ Entry point 
  *
  * Return: Always 0 (Success)
  */
+int main(void)
+
 {
 	(2,"and that piece of art is useful\"-Dora Korpa,2015-10-19\n", 59);
 	return(1);
