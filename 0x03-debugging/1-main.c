db6e0fbd7c73d68a9a0baf615bb92369f7f89aed
Smith dare <smithdare279@gmail.com>
2022-06-08 15:50:12 -0700

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,13 +12,11 @@ int i;
 printf("Infinite loop incoming :(\n");
 i = 0;
 
-/**
-*Avoid infiniteloop
-*	while (i < 10)
-*	{
-*		putchar(i);
-*	}
-*/
+Avoid infiniteloop
+	while (i < 10)
+	{
+		putchar(i);
+	}
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
