fbc3478029458ec257933ae8e75572fcecac11ee
Smith dare <smithdare279@gmail.com>
2022-06-07 20:52:44 -0700

    Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - computes and prints the sum of all the multiples
+ * main - computes and prints the sum of all multiples
  * of 3 or 5 below 1024
  * Return: Always 0 (Success)
  */
