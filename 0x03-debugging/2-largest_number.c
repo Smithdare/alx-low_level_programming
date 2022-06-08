9bfa03123a510ee7e46f11ac0a6ce71b47379755
Smith dare <smithdare279@gmail.com>
2022-06-08 16:24:12 -0700

    Task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -7,7 +7,7 @@
  * @c: third integer
  * Return: largest number
  */
-int largest_number(int a, int b, int c)
+int largest_number(int 'a', int 'b', int 'c')
 {
 	int largest;
 
@@ -23,5 +23,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
+
 	return (largest);
 }
