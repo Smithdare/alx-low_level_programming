88c3b08fa1670d32821bb16297ad493ab930c2e1
Smith dare <smithdare279@gmail.com>
2022-06-07 21:14:50 -0700

    Task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - finds and prints the sum of the even-valued terms
+ * main - finds and prints the sum of even-valued terms
  * followed by a new line
  * Return: Always 0 (Success)
  */
