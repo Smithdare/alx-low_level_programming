a27770c54850f7c3d149efb1790da5cb38851446
Smith dare <smithdare279@gmail.com>
2022-06-09 04:55:10 -0700

    t commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,14 @@
 #include "main.h"
 
 /**
- * _isupper - a function that checks for uppercase character
- *
- *@c: input
- *Return 1 if c is uppercase, 0 otherwise
- *
+ *  _isupper - a function that checks for uppercase character
+ * @c: input
+ * Return 1 if c is uppercase, 0 otherwise
  */
-
 int _isupper(int c)
-
 {
 		if ((c >= 'A') && (c <= 'Z'))
-
-		return (1);
-
+			return (1);
 		else
-	return (0);
+			return (0);
 }
