8c44f4d32407e14efaa1683be67897a0798786a9
Smith dare <smithdare279@gmail.com>
2022-06-09 18:36:37 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "main."
 
 /**
  *  _isupper - a function that checks for uppercase characters
@@ -8,7 +8,7 @@
 int _isupper(int c);
 
 {
-	if ((c >= 65 && c <= 90))
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
