4bfe7be1107d2791eb744515b0abf522ceeaef48
Smith dare <smithdare279@gmail.com>
2022-06-09 18:33:55 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
-
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -14,4 +10,3 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-#endif /* MAIN_H */
