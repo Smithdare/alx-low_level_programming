0d91b6d24bb45688d7f8ca6cff0a4c819b4ed1f6
Smith dare <smithdare279@gmail.com>
2022-06-21 03:55:54 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,6 +13,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int index;
 	char *memory = s, value = b;
+
 	for (index = 0; index < n; index++)
 		memory[index] = value;
 	return (memory);
