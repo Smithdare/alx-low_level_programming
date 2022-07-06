b9558819df1c941ed0a05cfb4abb2015c1c05007
Smith dare <smithdare279@gmail.com>
2022-07-05 21:13:57 -0700

    Task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,11 +7,14 @@
  * @d: struct dog to print
  */
 void print_dog(struct dog *d)
+{
 	if (d == NULL)
 		return;
+
 	if (d->name == NULL)
 		d->name = "(nil)";
 	if (d->owner == NULL)
 		d->owner = "(nil)";
+
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
