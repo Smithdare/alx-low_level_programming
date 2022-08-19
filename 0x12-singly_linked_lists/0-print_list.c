6f3574a310a9ea2200b5855103622cf61c1d90a2
Smith dare <smithdare279@gmail.com>
2022-08-19 16:40:40 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,6 @@
  */
 size_t print_list(const list_t *h)
 {
-
 	size_t nodes = 0;
 
 	while (h)
@@ -19,12 +18,8 @@ size_t print_list(const list_t *h)
 
 		else
 			printf("[%d] %s\n", h->len, h->str);
-
 		nodes++;
 		h = h->next;
-
 	}
-
 	return (nodes);
-
 }
