1fbb93b5d77e917f6d1c45dcbafd929f809a2517
Smith dare <smithdare279@gmail.com>
2022-08-19 12:54:02 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,22 @@
  */
 size_t print_list(const list_t *h)
 {
+
 	size_t nodes = 0;
 
 	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
+
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
 		nodes++;
 		h = h->next;
+
 	}
+
 	return (nodes);
+
 }
