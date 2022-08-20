a91d88a72ffe6cd0942171d7a48c835ef29024cf
Smith dare <smithdare279@gmail.com>
2022-08-19 17:03:53 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,21 +14,21 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	char *dup;
 	int len;
-
 	list_t *new, *last;
+
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-	dup = strdup(str);
 
+	dup = strdup(str);
 	if (str == NULL)
 	{
 		free(new);
-
 		return (NULL);
 	}
 	for (len = 0; str[len];)
 		len++;
+
 	new->str = dup;
 	new->len = len;
 	new->next = NULL;
